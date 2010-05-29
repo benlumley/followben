69c47e253869a5c8721c9b7421590f72f2d1a00c
Ben Lumley <ben@benlumley.co.uk>
2010-05-29 17:04:06 +0100

    Default module

---
+++ apps/frontend/modules/default/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * default actions.
+ *
+ * @package    followben
+ * @subpackage default
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class defaultActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+
+  }
+}
