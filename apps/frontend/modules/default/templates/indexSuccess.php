7b25f2fd7c22403a5528de5de81c7717d6e746d8
Ben Lumley <ben@benlumley.co.uk>
2010-05-31 15:31:22 +0100

    added design, work on map

--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -1,33 +1,34 @@
 <div class="container ">
   <div class="span-12">
-    <h1>Follow Ben</h1>
+    <img src="images/logo.png" alt="Follow Ben">
   </div>
-  <div class="span-12 last">
-  </div>
-  <div class="clear"></div>
-
-  <div class="span-6">
-    <h4>What Am I Doing?</h4>
-    <p>I am cycling from Lands End to John O'Groats in aid of Friends of Bristol Haematology and Oncology Centre</p>
+  <div class="span-12 last" id="justgiving">
     <a href= 'http://www.justgiving.com/ben-lumley' alt='JustGiving - Sponsor me!' target='_blank'> <img src='/images/justgiving.png' width='230' height='50'> </a>
   </div>
-  <div class="span-6">
-    <h4>What's this?</h4>
+  <div class="clear"></div>
+
+  <div class="span-6 blob">
+    <h2>What Am I Doing?</h2>
+    <p>I am cycling from Lands End to John O'Groats in aid of Friends of Bristol Haematology and Oncology Centre</p>
+    
+  </div>
+  <div class="span-6 blob">
+    <h2>What's this?</h2>
     <p>Its a map that will show.</p>
   </div>
-  <div class="span-6">
-    <h4>Follow Me</h4>
+  <div class="span-6 blob">
+    <h2>Follow Me</h2>
     <p>I am cycling from Lands End to John O'Groats in aid of the Friends of Bristol Haematology and Oncology Centre at the BRI in Bristol</p>
     <p>This map will let you keep an eye on my progress, whilst subtly convincing you to sponsor me!</p>
   </div>
-  <div class="span-6 last">
-    <h4>Sponsor Me</h4>
+  <div class="span-6 blob last">
+    <h2>Sponsor Me</h2>
     <p>I am cycling from Lands End to John O'Groats in aid of the Friends of Bristol Haematology and Oncology Centre at the BRI in Bristol</p>
     <p>This map will let you keep an eye on my progress, whilst subtly convincing you to sponsor me!</p>
   </div>
 
+  <hr>
 
-  <div class="clear"></div>
   <div class="span-6 ">
     <iframe src="http://www.facebook.com/plugins/livefeed.php?app_id=255955255198&amp;width=230&amp;height=600&amp;xid" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:230px; height:600px;" allowTransparency="true"></iframe>
     <fb:comments numposts="20" width="230"></fb:comments>
--- web/js/main.js
+++ web/js/main.js
@@ -22,8 +22,15 @@ var destination = null;
 var waypoints = [];
 var markers = [];
 var map = null;
+var bounds = null;
+var directionsService = null;
+var dirDisplayOptions = {
+      markerOptions: { icon: "/images/arrowno.png" }
+    }
+
 
 function mapSetup() {
+
     var myLatlng = new google.maps.LatLng(54, -4);
     var myOptions = {
       zoom: 5,
@@ -32,7 +39,10 @@ function mapSetup() {
       overviewControlOptions: { enabled: true },
       mapTypeId: google.maps.MapTypeId.TERRAIN
     }
+    bounds = new google.maps.LatLngBounds();
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    directionsService = new google.maps.DirectionsService();
 }
 
 
@@ -44,30 +54,84 @@ function populateMap(date) {
     $.each(data.points, function(i,point){
       addWaypoint(point);
     });
+    //map.panToBounds(bounds);
+    applyRoute();
   })
 
 }
 
 function addWaypoint(point) {
   latLng = new google.maps.LatLng(point.latitude, point.longitude);
-  if (origin == null) {
-    origin = latLng;
-    addMarker(origin);
-  } else if (destination == null) {
-    destination = latLng;
-    addMarker(destination);
-  } else {
-    waypoints.push({ location: destination, stopover: true });
-    destination = latLng;
-    addMarker(destination);
-  }
+//  bounds.extend(latLng);
+
+  waypoints.push({ location: latLng, stopover: true });
 }
 
 function addMarker(latlng) {
   markers.push(new google.maps.Marker({
     position: latlng,
     map: map,
-    icon: "/images/blue-dot.png",
+    icon: "/images/arrowno.png",
     title: "test"
   }));
 }
+
+
+function applyRoute() {
+
+    using_waypoints = [];
+    z=0;
+    for (i in waypoints) {
+      if (origin == null) {
+        origin = waypoints[i].location;
+      } else if (using_waypoints.length < 8) {
+        using_waypoints.push(waypoints[i]);
+      } else {
+        destination = waypoints[i].location;
+
+        loadRoute(origin, destination, using_waypoints);
+
+        using_waypoints = [];
+        origin = destination;
+        destination = null;
+        return;
+      }
+
+    }
+
+    console.log(using_waypoints);
+    if (using_waypoints.length) {
+      destination = using_waypoints.pop();
+      destination = destination.location;
+      loadRoute(origin, destination, using_waypoints);
+    }
+
+
+}
+
+
+function loadRoute(origin, destination, waypoints) {
+
+    var request = {
+      origin: origin,
+      destination: destination,
+      waypoints: waypoints,
+      travelMode: google.maps.DirectionsTravelMode.DRIVING,
+      optimizeWaypoints: false,
+      avoidHighways: false,
+      avoidTolls: false
+    };
+
+    directionsService.route(request, function(response, status) {
+      console.log(status);
+      if (status == google.maps.DirectionsStatus.OK) {
+        renderDirections(response);
+      }
+    });
+}
+
+function renderDirections(result) {
+  var directionsRenderer = new google.maps.DirectionsRenderer(dirDisplayOptions);
+  directionsRenderer.setMap(map);
+  directionsRenderer.setDirections(result);
+} 
