7cffecb6c9f119f9f702c64c1d525e0aca532809
Ben Lumley <ben@benlumley.co.uk>
2010-06-01 23:35:47 +0100

    work on maps v2 JS - map loads and puts on markers

--- web/js/main.js
+++ web/js/main.js
@@ -30,19 +30,17 @@ var dirDisplayOptions = {
 
 
 function mapSetup() {
-
-    var myLatlng = new google.maps.LatLng(54, -4);
-    var myOptions = {
-      zoom: 5,
-      center: myLatlng,
-      navigationControlOptions: {style: google.maps.NavigationControlStyle.ZOOM_PAN},
-      overviewControlOptions: { enabled: true },
-      mapTypeId: google.maps.MapTypeId.TERRAIN
-    }
-    bounds = new google.maps.LatLngBounds();
-    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
-    directionsService = new google.maps.DirectionsService();
+    var myLatlng = new GLatLng(54, -4);
+    bounds = new GLatLngBounds();
+    map = new GMap2(document.getElementById("map_canvas"));
+    map.addControl(new GMapTypeControl());
+    map.enableScrollWheelZoom();
+    map.addControl(new GLargeMapControl3D());
+    map.addControl(new GOverviewMapControl());
+    map.addMapType(G_NORMAL_MAP);
+    map.addMapType(G_PHYSICAL_MAP);
+    map.setMapType(G_PHYSICAL_MAP);
+    map.setCenter(myLatlng, 5);
 }
 
 
@@ -54,84 +52,66 @@ function populateMap(date) {
     $.each(data.points, function(i,point){
       addWaypoint(point);
     });
-    //map.panToBounds(bounds);
-    applyRoute();
+
+    map.panToBounds(bounds);
   })
 
 }
 
 function addWaypoint(point) {
-  latLng = new google.maps.LatLng(point.latitude, point.longitude);
-//  bounds.extend(latLng);
+  latlng = new GLatLng(point.latitude, point.longitude);
+  bounds.extend(latlng);
+  waypoints.push(latlng);
 
-  waypoints.push({ location: latLng, stopover: true });
-}
+  var icon= new GIcon();
 
-function addMarker(latlng) {
-  markers.push(new google.maps.Marker({
-    position: latlng,
-    map: map,
-    icon: "/images/arrowno.png",
-    title: "test"
-  }));
-}
+  var date = new Date(parseInt(point.timestamp) * 1000);
+  var description = '<table id="description"><tr><td>Device Name:</td><td>' + point.device_label + '</td></tr><tr><td>Date:</td><td>' + date.getDayName() + ', ' + date.getMonthName() + ' ' + date.getDate() + ', ' + date.getFullYear() + '</td></tr><tr><td>Time:</td><td>' + date.get12HourTime() + ':' + pad(date.getMinutes(), 2) + ':' +  pad(date.getSeconds(), 2) + ' ' + date.get12HourTimeSuffix() + ' ET</td></tr><tr><td>Speed:</td><td>' + point.speed + ' MPH</td></tr><tr><td>Latitude:</td><td>' + point.latitude + '</td></tr><tr><td>Longitude:</td><td>' + point.longitude + '</td></tr><tr><td>Distance:</td><td>' + point.distance + ' miles</td></tr><tr><td>Heading:</td><td>' + point.heading + ' degrees</td></tr><tr><td>Altitude:</td><td>' + point.altitude + ' feet</td></tr></table>';
 
+//  if(i == 0) {
+//    icon.image = "images/red-dot.png";
+//    icon.iconAnchor = new GPoint(16, 32);
+//    icon.iconSize = new GSize(30, 30);
+//    icon.infoWindowAnchor = new GPoint(16, 0);
+//  } else if(i == data.points.length-1) {
+//    icon.image = "images/green-dot.png";
+//    icon.iconAnchor = new GPoint(16, 32);
+//    icon.iconSize = new GSize(30, 30);
+//    icon.infoWindowAnchor = new GPoint(16, 0);
+//  } else {
+    icon.image = "images/arrowno.png";
+    icon.iconAnchor = new GPoint(8, 8);
+    icon.iconSize = new GSize(15, 15);
+    icon.infoWindowAnchor = new GPoint(8, 0);
+//  }
 
-function applyRoute() {
-
-    using_waypoints = [];
-    z=0;
-    for (i in waypoints) {
-      if (origin == null) {
-        origin = waypoints[i].location;
-      } else if (using_waypoints.length < 8) {
-        using_waypoints.push(waypoints[i]);
-      } else {
-        destination = waypoints[i].location;
-
-        loadRoute(origin, destination, using_waypoints);
-
-        using_waypoints = [];
-        origin = destination;
-        destination = null;
-        return;
-      }
-
-    }
-
-    console.log(using_waypoints);
-    if (using_waypoints.length) {
-      destination = using_waypoints.pop();
-      destination = destination.location;
-      loadRoute(origin, destination, using_waypoints);
-    }
-
+    icon.shadow = "";
+    var marker = new GMarker(latlng, { icon:icon });
+    marker.bindInfoWindowHtml(description);
+    map.addOverlay(marker);
 
 }
 
 
-function loadRoute(origin, destination, waypoints) {
-
-    var request = {
-      origin: origin,
-      destination: destination,
-      waypoints: waypoints,
-      travelMode: google.maps.DirectionsTravelMode.DRIVING,
-      optimizeWaypoints: false,
-      avoidHighways: false,
-      avoidTolls: false
-    };
-
-    directionsService.route(request, function(response, status) {
-      console.log(status);
-      if (status == google.maps.DirectionsStatus.OK) {
-        renderDirections(response);
-      }
-    });
+function pad(number, length) {
+  var str = '' + number;
+  while (str.length < length) {
+      str = '0' + str;
+  }
+  return str;
 }
 
-function renderDirections(result) {
-  var directionsRenderer = new google.maps.DirectionsRenderer(dirDisplayOptions);
-  directionsRenderer.setMap(map);
-  directionsRenderer.setDirections(result);
-} 
+Date.prototype.getMonthName = function() {
+  var m = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  return m[this.getMonth()];
+}
+Date.prototype.getDayName = function() {
+  var d = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  return d[this.getDay()];
+}
+Date.prototype.get12HourTime = function() {
+  return this.getHours()>12 ? this.getHours() - 12 : this.getHours();
+}
+Date.prototype.get12HourTimeSuffix = function() {
+  return this.getHours()>12 ? 'pm' : 'am';
+}
