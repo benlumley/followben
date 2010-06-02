b0c3cda4820620f579e71b7d81aa744a87896b5c
Steve <steve@netbook.stevelacey.net>
2010-06-02 23:56:22 +0100

    Abstracted Twitter username to app.yml and added method for displaying latest tweets.

--- lib/model/doctrine/TweetTable.class.php
+++ lib/model/doctrine/TweetTable.class.php
@@ -1,11 +1,15 @@
 <?php
 
+class TweetTable extends Doctrine_Table {
+  public static function getInstance() {
+    return Doctrine_Core::getTable('Tweet');
+  }
 
-class TweetTable extends Doctrine_Table
-{
-    
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('Tweet');
-    }
+  public function getLatest($limit = 10) {
+    return Doctrine_Query::create()->
+      from('Tweet')->
+      orderBy('id desc')->
+      limit($limit)->
+      execute();
+  }
 }
