9b58b59c431cb71fd7affaa13b3b88f90c8764eb
Ben Lumley <ben@benlumley.co.uk>
2010-06-13 14:41:59 +0100

    Failure 2 :(

---
+++ apps/frontend/modules/default/templates/newsSuccess.php
@@ -0,0 +1,19 @@
+<center>
+  <div style="width: 600px; text-align: left; font-size: 12px;">
+
+<h2>Bad News...</h2>
+
+<p>I’m afraid the cycling trip hasn’t quite worked out to plan. With the support car having ended up in the garage just hours before we left, it broke down again yesterday, and was temporarily fixed by the AA.  Meanwhile, it was another tough, hilly morning on the bikes, but by the afternoon, spirits were higher and we made it to Aztec West as planned. Unfortunately, on the way to our accommodation yesterday evening, one of the bikes flew off the bike rack on the motorway, and was seen bouncing across the lanes and over the central reservation into the other carriageway. Thankfully it caused no accidents, and so no-one was hurt, but we were all very shaken, downhearted and starting to feel that the trip was ill fated. Not to mention that we were a bike short and lacking a reliable way to transport bikes. </p>
+
+<p>We decided to sleep on it, but I’m afraid to say that we have decided today not to continue. Yesterday’s activities have really knocked our confidence and motivation, not to mention added new logistical challenges. </p>
+
+<p>We had the option of finding another bike and picking the route up a day or two down the line (to make use of booked accommodation and time off), but none of us felt that cycling “Lands End to John O Groats with a hole in the middle” quite had the same ring to it. For me, striving to complete the ride in 9 consecutive days was a large part of the challenge, and what kept me motivated when things were tough – but obviously now this isn’t going to be an option. The last two days have taught me that mental strength is a critical component of the ride and we have decided that we just don’t have enough left to make it – plus we can’t help but wonder what the next disaster will be. It wasn’t an easy decision, and to say we are disappointed is a real understatement. Nevertheless, we are still really pleased that we made it this far.</p>
+
+<p>However, with so much sponsorship kindly raised, and a week off work, we don’t want to completely give up. So we’ve set ourselves a 6 day challenge of different sporting activities – including climbing Snowdon, cycling to London, a swimathon, high ropes, canoeing, abseiling, rock climbing and anything else we can fit in (ideas welcome!).</p>
+
+<p>I really feel that I’ve let everyone one who has so generously sponsored and supported me down, and I really am sorry for that. Also, I’m afraid I can only apologise to our next two support teams who had so kindly committed time and money to helping us on the rest of our journey, and of course to Kate who put up with us for the two days we completed. I know our new plan is not quite the same, but I wanted to let you all know – and I completely understand it if you would like me to personally reimburse your sponsorship – please just let me know.</p>
+
+<p>Ben</p>
+
+  </div>
+</center>
